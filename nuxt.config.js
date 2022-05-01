3e3c9501e65cd8bcbb55614f59ef589e22468e4e
Sarad Gajurel <saradgajurel@gmail.com>
2022-05-01 20:30:53 +0545

    feat: sitemap and robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,4 +13,14 @@ export default theme({
         "Mega Dash is a programming cookbook with interactive examples and references of Flutter, Dart, Git as well as other various programming languages and tools.",
     },
   },
+  modules: ["@nuxtjs/sitemap"],
+  sitemap: {
+    hostname: "https://megadash.dev",
+    exclude: ["/releases"],
+    async routes() {
+      const { $content } = require("@nuxt/content");
+      const routes = await $content("en", { deep: true }).only(["to"]).fetch();
+      return routes.map((route) => route.to);
+    },
+  },
 });
